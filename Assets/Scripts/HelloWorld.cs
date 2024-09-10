8952d3915ea73f15c2e00db12c832b85d6e08d78
Bedrockbreaker <coolmandude101@gmail.com>
2024-09-10 16:28:30 -0700

    Scaffold project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+	
+	void Start() {
+		Debug.Log("Hello World!");
+	}
+
+	/* void Update() {
+		
+	} */
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TankGame
+
+> 戦車ゲーム
+
+A game created for GPE205 - Gameplay Programming Concepts.
+Built using Unity 6 (6000.0.18f1)
