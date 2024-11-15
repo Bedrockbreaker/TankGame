34fe8dfb31402ba6a4d026d7be806bbddebe7a64
Bedrockbreaker <coolmandude101@gmail.com>
2024-11-14 17:54:31 -0700

    WIP particles for different effects

---
+++ Assets/Scripts/StatusEffect/StatusEffectParticle.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatusEffectParticle", menuName = "Scriptable Objects/StatusEffectParticle")]
+public class StatusEffectParticle : ScriptableObject {
+
+	public ParticleSystem particlePrefab;
+}
